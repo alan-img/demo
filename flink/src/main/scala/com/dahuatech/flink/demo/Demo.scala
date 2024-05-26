de08c1917653a2fd820abc3263ef1b1c4b0f1c4f
qinjiawei <738437340@qq.com>
2024-05-26 14:43:12 +0800

    1、2024-05-26

---
+++ flink/src/main/scala/com/dahuatech/flink/demo/Demo.scala
@@ -0,0 +1,20 @@
+package com.dahuatech.flink.demo
+
+/**
+ * <p>projectName: demo</p>
+ * <p>packageName: com.dahuatech.flink.demo</p>
+ * <p>className: Demo</p>
+ * <p>date: 5/16/2024</p>
+ *
+ * @author qinjiawei(alan)
+ * @since JDK8.0
+ * @version 1.0.0
+ */
+
+object Demo {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}

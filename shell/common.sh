f3dc77619a7ba6538fbf146ff79cb7b418f155fe
qinjiawei <738437340@qq.com>
2024-07-11 15:55:19 +0800

    1、提交2024-7-8 17:05:04

--- shell/common.sh
+++ shell/common.sh
@@ -1,24 +1,18 @@
-namespace=cloudspace
-function get() { docker ps | grep $1 | grep -v pause | grep -v sidecar; }
-function toc()  { docker exec -it -u root -w / $(docker ps | grep $1 | grep -v pause | grep -v sidecar | head -n 1 | awk '{print $1}') /bin/bash; }
-
-function getall() { kubectl -n ${namespace} get po; }
-function getpod() { kubectl -n ${namespace} get pods -A -o wide | grep $1; }
-function epod() { kubectl -n ${namespace} edit pod $1; }
-function dpod() { kubectl -n ${namespace} delete po $1; }
-function top() { kubectl -n ${namespace} exec -it $(kubectl -n ${namespace} get pods | grep $1 | awk '{print $1}') -- /bin/bash; }
-
-function wpod() { kubectl -n ${namespace} get pods -w -o wide | grep $1; }
-function despod() { kubectl -n ${namespace} describe pod $(kubectl -n ${namespace} get pods | grep $1 | awk '{print $1}'); }
-
-function toppod() { kubectl -n ${namespace} top pod; }
-function logpod() { kubectl -n ${namespace} logs -f $(kubectl -n ${namespace} get pods | grep $1 | awk '{print $1}'); }
-
-function sdep() { kubectl -n ${namespace} get deploy | grep $1; }
-function edep() { kubectl -n ${namespace} edit deploy $1; }
-
-function ssts() { kubectl -n ${namespace} get sts | grep $1; }
-function ests() { kubectl -n ${namespace} edit sts $1; }
-
-function ssvc() { kubectl -n ${namespace} get svc | grep $1; }
-function esvc() { kubectl -n ${namespace} edit svc $1; }
+namespace=cloudspace;
+function s() { docker ps | grep $1 | grep -v pause | grep -v sidecar; };
+function to()  { docker exec -it -u root -w / $(docker ps | grep $1 | grep -v pause | grep -v sidecar | head -n 1 | awk '{print $1}') /bin/bash; };
+function sa() { kubectl -n ${namespace} get po; };
+function sp() { kubectl -n ${namespace} get pods -A -o wide | grep $1; };
+function ep() { kubectl -n ${namespace} edit pod $1; };
+function dp() { kubectl -n ${namespace} delete po $1; };
+function top() { kubectl -n ${namespace} exec -it $(kubectl -n ${namespace} get pods | grep $1 | awk '{print $1}') -- /bin/bash; };
+function sdep() { kubectl -n ${namespace} get deploy | grep $1; };
+function edep() { kubectl -n ${namespace} edit deploy $1; };
+function ssts() { kubectl -n ${namespace} get sts | grep $1; };
+function ests() { kubectl -n ${namespace} edit sts $1; };
+function ssvc() { kubectl -n ${namespace} get svc | grep $1; };
+function esvc() { kubectl -n ${namespace} edit svc $1; };
+function watpod() { kubectl -n ${namespace} get pods -w -o wide | grep $1; };
+function despod() { kubectl -n ${namespace} describe pod $(kubectl -n ${namespace} get pods | grep $1 | awk '{print $1}'); };
+function toppod() { kubectl -n ${namespace} top pod; };
+function logpod() { kubectl -n ${namespace} logs -f $(kubectl -n ${namespace} get pods | grep $1 | awk '{print $1}'); };

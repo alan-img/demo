8a6aa12a87f54fbcb5566cddcb42429c6c826c73
alan <738437340@qq.com>
2025-09-29 11:02:27 +0800

    -- 提交

--- spark/src/main/scala/com/dahuatech/spark/demo/SparkSubmitRemoteDemo.scala
+++ spark/src/main/scala/com/dahuatech/spark/demo/SparkSubmitRemoteDemo.scala
@@ -9,6 +9,11 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.util.LongAccumulator
 import org.slf4j.{Logger, LoggerFactory}
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Random
+import scala.collection.immutable
+
 /**
  * <p>projectName: demo</p>
  * <p>packageName: com.dahuatech.spark.demo</p>
@@ -23,29 +28,42 @@ import org.slf4j.{Logger, LoggerFactory}
 object SparkSubmitRemoteDemo {
   private val log: Logger = LoggerFactory.getLogger(this.getClass)
 
+  case class Demo(id: String, name: String, age: Long, city: String, province: String, dt: String)
+
   def main(args: Array[String]): Unit = {
+
     val sparkSession: SparkSession = SparkUtil.getSparkSession()
     import sparkSession.implicits._
 
-    val df = sparkSession.read
+    val dt = LocalDate.now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+
+    val df = sparkSession.range(0L, 10000)
+      .map(id => Demo(id.toString, generateRandomString(32), id, generateRandomString(32), generateRandomString(32), dt))
+      .toDF("id", "name", "age", "city", "province", "dt")
+
+    df.repartition(20).write
       .format("jdbc")
       .option("url", "jdbc:mysql://hadoop101:3306/demo?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8")
-      //      .option("dbtable", "(SELECT name, age FROM test) tmp") // 可写子查询，需加别名
-      .option("dbtable", "test") // 或者直接使用表名
+      .option("dbtable", "test")
       .option("user", "root")
       .option("password", "root")
       .option("driver", "com.mysql.jdbc.Driver")
-      // --- 大表分区并行读取 ---
-      .option("partitionColumn", "age")       // 分区字段（整型/时间戳）
-      .option("lowerBound", "1")             // 最小值
-      .option("upperBound", "1000000")       // 最大值
-      .option("numPartitions", "20")         // 并行连接数
-      // --- 优化参数 ---
-      .option("fetchsize", "1000")           // 批次抓取行数
-      .option("queryTimeout", "300")         // 查询超时（秒）
-      .load()
+      .option("batchsize", "500")                  // 每批次插入多少行，默认 1000
+      .option("isolationLevel", "READ_COMMITTED")  // 事务隔离级别
+      .mode(SaveMode.Overwrite)                    // 追加写入
+      .save()
 
-    println(df.count)
+  }
 
+  /**
+   * 生成指定长度的随机字符串 非BASE64字符串 因为BASE64字符串"="只会出现在末尾
+   *
+   * @param length
+   * @return
+   */
+  def generateRandomString(length: Int): String = {
+    val chars: immutable.IndexedSeq[Char] = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9') ++ Seq('/', '+')
+    val random = new Random
+    (1 to length).map(_ => chars(random.nextInt(chars.length))).mkString
   }
 }
